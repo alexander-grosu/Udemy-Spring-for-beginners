e085c525457cd95a5246f14e3f2309ea887de1d7
progerSapog <vlad.sapozhnickof@gmail.com>
2021-02-19 22:39:43 +0300

    82. Get one employee

---
+++ Section 6: Spring REST/80. Application configuration/src/main/java/com/zaurtregulov/spring/rest/controller/MyRestController.java
@@ -0,0 +1,7 @@
+package com.zaurtregulov.spring.rest.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController    //контроллер, управлеющий рест запросами и ответами
+public class MyRestController {
+}
